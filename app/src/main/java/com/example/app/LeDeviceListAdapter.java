4d3c22b301acf6ecba120abda586997f0c6fbdab
qzx <970595181@qq.com>
2020-11-16 21:25:53 +0800

    第一个版本，强度墙已经设置完毕

---
+++ app/src/main/java/com/example/app/LeDeviceListAdapter.java
@@ -0,0 +1,123 @@
+package com.example.app;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import com.example.app.iBeaconClass.iBeacon;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class LeDeviceListAdapter extends BaseAdapter {
+
+    // Adapter for holding devices found through scanning.
+
+    private ArrayList<iBeacon> mLeDevices;
+    private LayoutInflater mInflator;
+    private Activity mContext;
+
+    public LeDeviceListAdapter(Activity c) {
+        super();
+        mContext = c;
+        mLeDevices = new ArrayList<iBeacon>();
+        mInflator = mContext.getLayoutInflater();
+    }
+
+    public void addDevice(iBeacon device) {
+        if(device==null)
+            return;
+        if(device.rssi>=-70) {//强度墙
+            for (int i = 0; i < mLeDevices.size(); i++) {
+                String btAddress = mLeDevices.get(i).bluetoothAddress;
+                if (btAddress.equals(device.bluetoothAddress)) {
+
+                    mLeDevices.add(i + 1, device);
+                    mLeDevices.remove(i);
+                    return;
+                }
+            }
+            mLeDevices.add(device);
+        }
+
+    }
+
+    public iBeacon getDevice(int position) {
+        return mLeDevices.get(position);
+    }
+
+    public void clear() {
+        mLeDevices.clear();
+    }
+
+    @Override
+    public int getCount() {
+        return mLeDevices.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mLeDevices.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+    public ArrayList<iBeacon> getlist(){
+        return mLeDevices;
+    }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+        // General ListView optimization code.
+        if (view == null) {
+            view = mInflator.inflate(R.layout.listitem_device, null);
+            viewHolder = new ViewHolder();
+            viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
+            viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
+            viewHolder.deviceUUID= (TextView)view.findViewById(R.id.device_beacon_uuid);
+            viewHolder.deviceMajor_Minor=(TextView)view.findViewById(R.id.device_major_minor);
+            viewHolder.devicetxPower_RSSI=(TextView)view.findViewById(R.id.device_txPower_rssi);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        iBeacon device = mLeDevices.get(i);
+        final String deviceName = device.name;
+        if (deviceName != null && deviceName.length() > 0)
+            viewHolder.deviceName.setText(deviceName);
+        else
+            viewHolder.deviceName.setText(R.string.unknown_device);
+
+        viewHolder.deviceAddress.setText(device.bluetoothAddress);
+        viewHolder.deviceUUID.setText(device.proximityUuid);
+        viewHolder.deviceMajor_Minor.setText("major:"+device.major+",minor:"+device.minor);
+        viewHolder.devicetxPower_RSSI.setText("txPower:"+device.txPower+",rssi:"+device.rssi);
+        return view;
+    }
+
+    class ViewHolder {
+        TextView deviceName;
+        TextView deviceAddress;
+        TextView deviceUUID;
+        TextView deviceMajor_Minor;
+        TextView devicetxPower_RSSI;
+    }
+    static class sortById implements Comparator {
+
+        public int compare(Object o1, Object o2) {
+            if(((iBeacon)o1).rssi>((iBeacon)o2).rssi)
+                return 1;
+            return -1;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.app;
+
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+public class MainActivity extends ListActivity {
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private final static String UUID_KEY_DATA = "0000ffe1-0000-1000-8000-00805f9b34fb";
+
+    private LeDeviceListAdapter mLeDeviceListAdapter;
+    /**搜索BLE终端*/
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private boolean mScanning;
+    private Handler mHandler;
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 60000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHandler = new Handler();
+        // Use this check to determine whether BLE is supported on the device.  Then you can
+        // selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        //开启蓝牙
+        mBluetoothAdapter.enable();
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Initializes list view adapter.
+        mLeDeviceListAdapter = new LeDeviceListAdapter(this);
+        Collections.sort(mLeDeviceListAdapter.getlist(),new LeDeviceListAdapter.sortById());
+        setListAdapter(mLeDeviceListAdapter);
+        scanLeDevice(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);
+        mLeDeviceListAdapter.clear();
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                    invalidateOptionsMenu();
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+        invalidateOptionsMenu();
+    }
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+
+                    final iBeaconClass.iBeacon ibeacon = iBeaconClass.fromScanData(device,rssi,scanRecord);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mLeDeviceListAdapter.addDevice(ibeacon);
+                            mLeDeviceListAdapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            };
+
+}
